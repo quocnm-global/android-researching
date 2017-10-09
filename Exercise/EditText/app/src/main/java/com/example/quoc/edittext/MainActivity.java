04fa5792ebaa2b66d57f8af1b8176c6a778cf0e1
DESKTOP-QMFI7GO\Quoc <quoc.nm.global@gmail.com>
2017-10-09 20:20:16 +0700

    [Edit Text]

--- Exercise/EditText/app/src/main/java/com/example/quoc/edittext/MainActivity.java
+++ Exercise/EditText/app/src/main/java/com/example/quoc/edittext/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.quoc.edittext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtContent;
+    Button btnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtContent = findViewById(R.id.editText);
+        btnClick = findViewById(R.id.buttonClick);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = edtContent.getText().toString();
+                Toast.makeText(MainActivity.this, content, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
